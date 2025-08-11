ef5b621c033fcded523b2d8dbbe35b185f06795a
duda <mullerermesm@gmail.com>
2025-08-11 20:22:21 -0300

    atv

---
+++ atividadesIntro/aumento_salario.py
@@ -0,0 +1,3 @@
+salario= 3600.00
+aumento= salario*0.23
+print("aumento", aumento, "salario", (salario+aumento))
---
+++ atividadesIntro/metros_mili.py
@@ -0,0 +1,3 @@
+valor =float(input("Digite um valor em metros: "))
+mm=valor*1000
+print("valor em mm: ",mm)
